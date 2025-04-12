2760cd07fc6f8487d6e0129142fc6703e291fa7f
Leo-12515 <leo.victoriojuarez@gmail.com>
2025-04-12 01:01:25 -0400

    Implemented a sorted dictionary and a sorted character count

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_char
+from stats import sort_dict
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -8,7 +10,15 @@ def get_book_text(filepath):
 
 def main():
 	words = count_words(get_book_text('books/frankenstein.txt'))
-	print(f"{words} words found in the document")
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
+	print(f"----------- Word Count ---------- \nFound {words} total words")
+	chars = count_char(get_book_text('books/frankenstein.txt'))
+	sorted_list = sort_dict(chars)
+	print(f"--------- Character Count -------")
+	for char, count in sorted_list:
+		print(f"{char}: {count}")
+	print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -4,3 +4,18 @@ def count_words(text):
         for a in words:
                 count += 1
         return count
+
+def count_char(characters):
+	chars = characters.lower()
+	chars_dict = {}
+	for i in chars:
+		if i in chars_dict:
+    			chars_dict[i] = chars_dict[i] + 1
+		else:
+    			chars_dict[i] = 1
+	return chars_dict
+
+def sort_dict(dict):
+	sorting = {char: count for char, count in dict.items() if char.isalpha()}
+	sorted_chars = sorted(sorting.items(), key=lambda pair: pair[1], reverse=True)
+	return sorted_chars
